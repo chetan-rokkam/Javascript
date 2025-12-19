451b267c5934f9568be6fdccf9ca5cea63358cb2
chetan-rokkam <chetanrokkam06@gmail.com>
2025-12-19 18:17:39 +0530

    Implement grade determination using ternary operator

---
+++ ternary.js
@@ -0,0 +1,20 @@
+let testScore = 99;
+let myGrade =
+ 
+testScore > 89
+? "A"
+
+:testScore > 89
+? "B"
+
+:testScore > 89
+? "C"
+
+:testScore > 89
+? "D"
+
+:testScore > 89
+? "E"
+: "F";
+
+console.log(`My test grade is: ${myGrade}`);
